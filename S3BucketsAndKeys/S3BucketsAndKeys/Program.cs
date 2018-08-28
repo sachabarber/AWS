8622ff942997863d4802446a3f64f16289ebe778
sacha barber <sbarber@nephilaadvisors.co.uk>
2018-08-28 21:31:08 +0100

    S3BucketsAndKeys

---
+++ S3BucketsAndKeys/S3BucketsAndKeys/Program.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.IO;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+
+namespace S3BucketsAndKeys
+{
+    class S3Sample
+    {
+        // Change the AWSProfileName to the profile you want to use in the App.config file.
+        // See http://aws.amazon.com/credentials  for more details.
+        // You must also sign up for an Amazon S3 account for this to work
+        // See http://aws.amazon.com/s3/ for details on creating an Amazon S3 account
+        // Change the bucketName and keyName fields to values that match your bucketname and keyname
+        static string bucketName = "some-nice-lower-case-bucket";
+        static string keyName = "simpletextkey";
+        static IAmazonS3 client;
+
+        public static void Main(string[] args)
+        {
+            if (CheckRequiredFields())
+            {
+                using (client = new AmazonS3Client())
+                {
+                    Console.WriteLine("Listing buckets");
+                    ListingBuckets();
+
+                    Console.WriteLine("Creating a bucket");
+                    CreateABucket();
+
+                    Console.WriteLine("Writing an object");
+                    WritingAnObject();
+
+                    Console.WriteLine("Writing an object only first time");
+                    WritingAnObjectOnce();
+
+                    Console.WriteLine("Writing an object using stream and TransferUtility");
+                    WritingAStream();
+
+                    Console.WriteLine("Reading an object");
+                    ReadingAnObject();
+
+                    Console.WriteLine("Deleting an object");
+                    DeletingAnObject();
+
+                    Console.WriteLine("Listing objects");
+                    ListingObjects();
+                }
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        static bool CheckRequiredFields()
+        {
+            NameValueCollection appConfig = ConfigurationManager.AppSettings;
+
+            if (string.IsNullOrEmpty(appConfig["AWSProfileName"]))
+            {
+                Console.WriteLine("AWSProfileName was not set in the App.config file.");
+                return false;
+            }
+            if (string.IsNullOrEmpty(bucketName))
+            {
+                Console.WriteLine("The variable bucketName is not set.");
+                return false;
+            }
+            if (string.IsNullOrEmpty(keyName))
+            {
+                Console.WriteLine("The variable keyName is not set.");
+                return false;
+            }
+
+            return true;
+        }
+
+
+
+        static bool S3FileExists(string bucketName, string keyName)
+        {
+            var s3FileInfo = new Amazon.S3.IO.S3FileInfo(client, bucketName, keyName);
+            return s3FileInfo.Exists;
+        }
+
+        static void ListingBuckets()
+        {
+            try
+            {
+                ListBucketsResponse response = client.ListBuckets();
+                foreach (S3Bucket bucket in response.Buckets)
+                {
+                    Console.WriteLine("You own Bucket with name: {0}", bucket.BucketName);
+                }
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                if (amazonS3Exception.ErrorCode != null &&
+                    (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId") ||
+                    amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    Console.WriteLine("Please check the provided AWS Credentials.");
+                    Console.WriteLine("If you haven't signed up for Amazon S3, please visit http://aws.amazon.com/s3");
+                }
+                else
+                {
+                    Console.WriteLine("An Error, number {0}, occurred when listing buckets with the message '{1}", amazonS3Exception.ErrorCode, amazonS3Exception.Message);
+                }
+            }
+        }
+
+        static void CreateABucket()
+        {
+            try
+            {
+
+                if (client.DoesS3BucketExist(bucketName))
+                {
+                    Console.WriteLine($"{bucketName} already exists, skipping this step");
+                }
+
+                PutBucketRequest request = new PutBucketRequest();
+                request.BucketName = bucketName;
+                client.PutBucket(request);
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                if (amazonS3Exception.ErrorCode != null && (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId") || amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    Console.WriteLine("Please check the provided AWS Credentials.");
+                    Console.WriteLine("If you haven't signed up for Amazon S3, please visit http://aws.amazon.com/s3");
+                }
+                else
+                {
+                    Console.WriteLine("An Error, number {0}, occurred when creating a bucket with the message '{1}", amazonS3Exception.ErrorCode, amazonS3Exception.Message);
+                }
+            }
+        }
+
+        static void WritingAnObject()
+        {
+            try
+            {
+                // simple object put
+                PutObjectRequest request = new PutObjectRequest()
+                {
+                    ContentBody = "this is a test",
+                    BucketName = bucketName,
+                    Key = keyName
+                };
+
+                PutObjectResponse response = client.PutObject(request);
+
+                // put a more complex object with some metadata and http headers.
+                PutObjectRequest titledRequest = new PutObjectRequest()
+                {
+                    BucketName = bucketName,
+                    Key = keyName
+                };
+                titledRequest.Metadata.Add("title", "the title");
+
+                client.PutObject(titledRequest);
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                if (amazonS3Exception.ErrorCode != null &&
+                    (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId") ||
+                    amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    Console.WriteLine("Please check the provided AWS Credentials.");
+                    Console.WriteLine("If you haven't signed up for Amazon S3, please visit http://aws.amazon.com/s3");
+                }
+                else
+                {
+                    Console.WriteLine("An error occurred with the message '{0}' when writing an object", amazonS3Exception.Message);
+                }
+            }
+        }
+
+        static void WritingAStream()
+        {
+
+            Stream GenerateStreamFromString(string s)
+            {
+                var stream = new MemoryStream();
+                var writer = new StreamWriter(stream);
+                writer.Write(s);
+                writer.Flush();
+                stream.Position = 0;
+                return stream;
+            }
+
+            try
+            {
+                var fileTransferUtility = new TransferUtility(client);
+                var fileName = Guid.NewGuid().ToString("N");
+                using (var fileToUpload = GenerateStreamFromString("some random string contents"))
+                {
+                    fileTransferUtility.UploadAsync(fileToUpload, bucketName, keyName);
+                }
+                Console.WriteLine($"Upload using stream to file '{fileName}' completed");
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                if (amazonS3Exception.ErrorCode != null &&
+                    (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId") ||
+                    amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    Console.WriteLine("Please check the provided AWS Credentials.");
+                    Console.WriteLine("If you haven't signed up for Amazon S3, please visit http://aws.amazon.com/s3");
+                }
+                else
+                {
+                    Console.WriteLine("An error occurred with the message '{0}' when writing an object", amazonS3Exception.Message);
+                }
+            }
+        }
+
+
+        static void WritingAnObjectOnce()
+        {
+            try
+            {
+
+                string uniqueKeyName = Guid.NewGuid().ToString("N");
+
+                for (int i = 0; i < 2; i++)
+                {
+                    if (!S3FileExists(bucketName, uniqueKeyName))
+                    {
+
+                        // simple object put
+                        Console.WriteLine($"Adding file {uniqueKeyName}");
+                        PutObjectRequest request = new PutObjectRequest()
+                        {
+                            ContentBody = "this is a test",
+                            BucketName = bucketName,
+                            Key = uniqueKeyName
+                        };
+
+                        PutObjectResponse response = client.PutObject(request);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"File {uniqueKeyName} existed");
+                    }
+                }
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                if (amazonS3Exception.ErrorCode != null &&
+                    (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId") ||
+                    amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    Console.WriteLine("Please check the provided AWS Credentials.");
+                    Console.WriteLine("If you haven't signed up for Amazon S3, please visit http://aws.amazon.com/s3");
+                }
+                else
+                {
+                    Console.WriteLine("An error occurred with the message '{0}' when writing an object", amazonS3Exception.Message);
+                }
+            }
+        }
+
+
+        static void ReadingAnObject()
+        {
+            try
+            {
+                GetObjectRequest request = new GetObjectRequest()
+                {
+                    BucketName = bucketName,
+                    Key = keyName
+                };
+
+                using (GetObjectResponse response = client.GetObject(request))
+                {
+                    string title = response.Metadata["x-amz-meta-title"];
+                    Console.WriteLine("The object's title is {0}", title);
+                    string dest = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), keyName);
+                    if (!File.Exists(dest))
+                    {
+                        response.WriteResponseStreamToFile(dest);
+                    }
+                }
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                if (amazonS3Exception.ErrorCode != null &&
+                    (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId") ||
+                    amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    Console.WriteLine("Please check the provided AWS Credentials.");
+                    Console.WriteLine("If you haven't signed up for Amazon S3, please visit http://aws.amazon.com/s3");
+                }
+                else
+                {
+                    Console.WriteLine("An error occurred with the message '{0}' when reading an object", amazonS3Exception.Message);
+                }
+            }
+        }
+
+        static void DeletingAnObject()
+        {
+            try
+            {
+                DeleteObjectRequest request = new DeleteObjectRequest()
+                {
+                    BucketName = bucketName,
+                    Key = keyName
+                };
+
+                client.DeleteObject(request);
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                if (amazonS3Exception.ErrorCode != null &&
+                    (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId") ||
+                    amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    Console.WriteLine("Please check the provided AWS Credentials.");
+                    Console.WriteLine("If you haven't signed up for Amazon S3, please visit http://aws.amazon.com/s3");
+                }
+                else
+                {
+                    Console.WriteLine("An error occurred with the message '{0}' when deleting an object", amazonS3Exception.Message);
+                }
+            }
+        }
+
+        static void ListingObjects()
+        {
+            try
+            {
+                ListObjectsRequest request = new ListObjectsRequest();
+                request.BucketName = bucketName;
+                ListObjectsResponse response = client.ListObjects(request);
+                foreach (S3Object entry in response.S3Objects)
+                {
+                    Console.WriteLine("key = {0} size = {1}", entry.Key, entry.Size);
+                }
+
+                // list only things starting with "foo"
+                request.Prefix = "foo";
+                response = client.ListObjects(request);
+                foreach (S3Object entry in response.S3Objects)
+                {
+                    Console.WriteLine("key = {0} size = {1}", entry.Key, entry.Size);
+                }
+
+                // list only things that come after "bar" alphabetically
+                request.Prefix = null;
+                request.Marker = "bar";
+                response = client.ListObjects(request);
+                foreach (S3Object entry in response.S3Objects)
+                {
+                    Console.WriteLine("key = {0} size = {1}", entry.Key, entry.Size);
+                }
+
+                // only list 3 things
+                request.Prefix = null;
+                request.Marker = null;
+                request.MaxKeys = 3;
+                response = client.ListObjects(request);
+                foreach (S3Object entry in response.S3Objects)
+                {
+                    Console.WriteLine("key = {0} size = {1}", entry.Key, entry.Size);
+                }
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                if (amazonS3Exception.ErrorCode != null && (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId") || amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    Console.WriteLine("Please check the provided AWS Credentials.");
+                    Console.WriteLine("If you haven't signed up for Amazon S3, please visit http://aws.amazon.com/s3");
+                }
+                else
+                {
+                    Console.WriteLine("An error occurred with the message '{0}' when listing objects", amazonS3Exception.Message);
+                }
+            }
+        }
+    }
+}

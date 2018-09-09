355fc4df014080a2a826bbc57dbcb50b6d58bc2f
sacha barber <sbarber@nephilaadvisors.co.uk>
2018-09-09 21:06:58 +0100

    Adding s3 TransferUtility

---
+++ Storage/S3TrasferUtility/S3TrasferUtility/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+using Nito.AsyncEx;
+
+namespace S3TransferUtility
+{
+    class Program
+    {
+        // Change the AWSProfileName to the profile you want to use in the App.config file.
+        // See http://aws.amazon.com/credentials  for more details.
+        // You must also sign up for an Amazon S3 account for this to work
+        // See http://aws.amazon.com/s3/ for details on creating an Amazon S3 account
+        // Change the bucketName and keyName fields to values that match your bucketname and keyname
+        static string bucketName = "some-nice-transferutility-bucket";
+        static string fileName = $"{Guid.NewGuid().ToString("N")}.txt";
+        static IAmazonS3 client;
+
+        private static void Main(string[] args)
+        {
+            var program = new Program();
+            AsyncContext.Run(() => program.MainAsync(args));
+        }
+
+        async void MainAsync(string[] args)
+        {
+            if (CheckRequiredFields())
+            {
+                using (client = new AmazonS3Client())
+                {
+                    var fileTransferUtility = new TransferUtility(client);
+
+                    Console.WriteLine("Writing an object using stream");
+                    await WritingAStreamPublicAsync();
+
+                    Console.WriteLine("Reading an object from S3 as stream");
+                    await ReadingAnObjectFromS3AsAStream();
+
+                    Console.WriteLine("Downloadinng an object from S3");
+                    await DownloadingAnObjectFromS3AsAStream();
+                }
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        async Task WritingAStreamPublicAsync()
+        {
+            await CarryOutAWSTask(async () =>
+            {
+                //sly inner function
+                async Task<Stream> GenerateStreamFromStringAsync(string s)
+                {
+                    var stream = new MemoryStream();
+                    var writer = new StreamWriter(stream);
+                    await writer.WriteAsync(s);
+                    await writer.FlushAsync();
+                    stream.Position = 0;
+                    return stream;
+                }
+
+                var bucketToCreate = $"public-{bucketName}";
+                await CreateABucketAsync(bucketToCreate);
+
+                var fileTransferUtility = new TransferUtility(client);
+                var contentsToUpload = "some random string contents";
+                Console.WriteLine("Uploading the following contents using TransferUtility");
+                Console.WriteLine(contentsToUpload);
+                using (var streamToUpload = await GenerateStreamFromStringAsync(contentsToUpload))
+                {
+                    var uploadRequest = new TransferUtilityUploadRequest()
+                    {
+                        InputStream = streamToUpload,
+                        Key = fileName,
+                        BucketName = bucketToCreate,
+                        CannedACL = S3CannedACL.PublicRead
+                    };
+
+                    //If you are uploading large files, TransferUtility 
+                    //will use multipart upload to fulfill the request
+                    await fileTransferUtility.UploadAsync(uploadRequest);
+                }
+                Console.WriteLine($"Upload using stream to file '{fileName}' completed");
+
+
+            }, "Writing using a Stream to public file");
+        }
+
+        async Task ReadingAnObjectFromS3AsAStream()
+        {
+            await CarryOutAWSTask(async () =>
+            {
+                var bucketToReadFrom = $"public-{bucketName}";
+
+                var fileTransferUtility = new TransferUtility(client);
+
+                using (var fs = await fileTransferUtility.OpenStreamAsync(bucketToReadFrom, fileName, CancellationToken.None))
+                {
+                    using (var reader = new StreamReader(fs))
+                    {
+                        var contents = await reader.ReadToEndAsync();
+                        Console.WriteLine($"Content of file {fileName} is");
+                        Console.WriteLine(contents);
+                    }
+                }
+            }, "Reading an Object from S3 as a Stream");
+        }
+
+        async Task DownloadingAnObjectFromS3AsAStream()
+        {
+            await CarryOutAWSTask(async () =>
+            {
+                var bucketToReadFrom = $"public-{bucketName}";
+                var fileTransferUtility = new TransferUtility(client);
+                string aTempFile = Path.Combine(Path.GetTempPath(), "SavedS3TextFile.txt");
+                await fileTransferUtility.DownloadAsync(aTempFile, bucketToReadFrom, fileName);
+                using (var fs = new FileStream(aTempFile, FileMode.Open))
+                {
+                    using (var reader = new StreamReader(fs))
+                    {
+                        var contents = await reader.ReadToEndAsync();
+                        Console.WriteLine($"Content of saved file {aTempFile} is");
+                        Console.WriteLine(contents);
+                    }
+                }
+            }, "Downloading an Object from S3 as a Stream");
+        }
+
+        bool CheckRequiredFields()
+        {
+            NameValueCollection appConfig = ConfigurationManager.AppSettings;
+
+            if (string.IsNullOrEmpty(appConfig["AWSProfileName"]))
+            {
+                Console.WriteLine("AWSProfileName was not set in the App.config file.");
+                return false;
+            }
+            if (string.IsNullOrEmpty(bucketName))
+            {
+                Console.WriteLine("The variable bucketName is not set.");
+                return false;
+            }
+            if (string.IsNullOrEmpty(fileName))
+            {
+                Console.WriteLine("The variable fileName is not set.");
+                return false;
+            }
+
+            return true;
+        }
+
+        async Task CreateABucketAsync(string bucketToCreate, bool isPublic = true)
+        {
+            await CarryOutAWSTask(async () =>
+            {
+                if (client.DoesS3BucketExist(bucketToCreate))
+                {
+                    Console.WriteLine($"{bucketToCreate} already exists, skipping this step");
+                }
+
+                PutBucketRequest putBucketRequest = new PutBucketRequest()
+                {
+                    BucketName = bucketToCreate,
+                    BucketRegion = S3Region.EUW2,
+                    CannedACL = isPublic ? S3CannedACL.PublicRead : S3CannedACL.Private
+                };
+                var response = await client.PutBucketAsync(putBucketRequest);
+            }, "Create a bucket");
+        }
+
+        async Task CarryOutAWSTask(Func<Task> taskToPerform, string op)
+        {
+            try
+            {
+                await taskToPerform();
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                if (amazonS3Exception.ErrorCode != null &&
+                    (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId") ||
+                    amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    Console.WriteLine("Please check the provided AWS Credentials.");
+                    Console.WriteLine("If you haven't signed up for Amazon S3, please visit http://aws.amazon.com/s3");
+                }
+                else
+                {
+                    Console.WriteLine($"An Error, number '{amazonS3Exception.ErrorCode}', " + 
+                                      $"occurred when '{op}' with the message '{amazonS3Exception.Message}'");
+                }
+            }
+        }
+    }
+}

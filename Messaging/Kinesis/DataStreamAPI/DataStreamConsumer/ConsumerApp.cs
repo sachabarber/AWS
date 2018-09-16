76a45179b6c7da027d474e23ff8528663a9eb83e
sacha barber <sbarber@nephilaadvisors.co.uk>
2018-09-16 20:33:26 +0100

    Kinesis stuff working

---
+++ Messaging/Kinesis/DataStreamAPI/DataStreamConsumer/ConsumerApp.cs
@@ -0,0 +1,68 @@
+﻿using Amazon.Kinesis.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon.Kinesis.DataStreamConsumer
+{
+    /// <summary>
+    /// A sample producer of Kinesis records.
+    /// </summary>
+    class ConsumerApp
+    {
+
+
+        private static readonly AmazonKinesisClient kinesisClient = new AmazonKinesisClient(RegionEndpoint.EUWest2);
+        const string myStreamName = "myTestStream";
+
+        public static void Main(string[] args)
+        {
+            new ConsumerApp().ReadFromStream().GetAwaiter().GetResult();
+        }
+
+        private async Task ReadFromStream()
+        {
+
+            DescribeStreamRequest describeRequest = new DescribeStreamRequest();
+            describeRequest.StreamName = myStreamName;
+
+            DescribeStreamResponse describeResponse = await kinesisClient.DescribeStreamAsync(describeRequest);
+            List<Shard> shards = describeResponse.StreamDescription.Shards;
+
+            foreach (Shard shard in shards)
+            {
+                GetShardIteratorRequest iteratorRequest = new GetShardIteratorRequest();
+                iteratorRequest.StreamName = myStreamName;
+                iteratorRequest.ShardId = shard.ShardId;
+                iteratorRequest.ShardIteratorType = ShardIteratorType.TRIM_HORIZON;
+
+                GetShardIteratorResponse iteratorResponse = await kinesisClient.GetShardIteratorAsync(iteratorRequest);
+                string iteratorId = iteratorResponse.ShardIterator;
+
+                while (!string.IsNullOrEmpty(iteratorId))
+                {
+                    GetRecordsRequest getRequest = new GetRecordsRequest();
+                    getRequest.Limit = 1000;
+                    getRequest.ShardIterator = iteratorId;
+
+                    GetRecordsResponse getResponse = await kinesisClient.GetRecordsAsync(getRequest);
+                    string nextIterator = getResponse.NextShardIterator;
+                    List<Record> records = getResponse.Records;
+
+                    if (records.Count > 0)
+                    {
+                        Console.WriteLine("Received {0} records. ", records.Count);
+                        foreach (Record record in records)
+                        {
+                            string theMessage = Encoding.UTF8.GetString(record.Data.ToArray());
+                            Console.WriteLine("message string: " + theMessage);
+                        }
+                    }
+                    iteratorId = nextIterator;
+                }
+            }
+        }
+
+    }
+}
---
+++ Messaging/Kinesis/DataStreamAPI/DataStreamProducer/DataStreamProducerApp.cs
@@ -0,0 +1,113 @@
+﻿using Amazon.Kinesis.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Amazon.Kinesis.DataStreamproducer
+{
+    /// <summary>
+    /// A sample producer of Kinesis records.
+    /// </summary>
+    class ProducerApp
+    {
+
+
+        private static readonly AmazonKinesisClient kinesisClient = new AmazonKinesisClient(RegionEndpoint.EUWest2);
+        const string myStreamName = "myTestStream";
+
+        public static void Main(string[] args)
+        {
+            new ProducerApp().WriteToStream().GetAwaiter().GetResult();
+        }
+
+        private async Task WriteToStream()
+        {
+
+            const string myStreamName = "myTestStream";
+            const int myStreamSize = 1;
+
+
+
+            try
+            {
+                var createStreamRequest = new CreateStreamRequest();
+                createStreamRequest.StreamName = myStreamName;
+                createStreamRequest.ShardCount = myStreamSize;
+                var createStreamReq = createStreamRequest;
+
+                var existingStreams = kinesisClient.ListStreamsAsync().Result;
+
+                if (!existingStreams.StreamNames.Contains(myStreamName))
+                {
+
+                    var CreateStreamResponse = kinesisClient.CreateStreamAsync(createStreamReq).Result;
+                    Console.WriteLine("Created Stream : " + myStreamName);
+                }
+            }
+            catch (ResourceInUseException)
+            {
+                Console.Error.WriteLine("Producer is quitting without creating stream " + myStreamName +
+                    " to put records into as a stream of the same name already exists.");
+                Environment.Exit(1);
+            }
+
+            await WaitForStreamToBecomeAvailableAsync(myStreamName);
+
+            Console.Error.WriteLine("Putting records in stream : " + myStreamName);
+            // Write 10 UTF-8 encoded records to the stream.
+            for (int j = 0; j < 10; ++j)
+            {
+                byte[] dataAsBytes = Encoding.UTF8.GetBytes("testdata-" + j);
+                using (MemoryStream memoryStream = new MemoryStream(dataAsBytes))
+                {
+                    try
+                    {
+                        PutRecordRequest requestRecord = new PutRecordRequest();
+                        requestRecord.StreamName = myStreamName;
+                        requestRecord.PartitionKey = "url-response-times";
+                        requestRecord.Data = memoryStream;
+
+                        PutRecordResponse responseRecord = await kinesisClient.PutRecordAsync(requestRecord);
+                        Console.WriteLine("Successfully sent record to Kinesis. Sequence number: {0}", responseRecord.SequenceNumber);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Failed to send record to Kinesis. Exception: {0}", ex.Message);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+
+        /// <summary>
+        /// This method waits a maximum of 10 minutes for the specified stream to become active.
+        /// <param name="myStreamName">Name of the stream whose active status is waited upon.</param>
+        /// </summary>
+        private static async Task WaitForStreamToBecomeAvailableAsync(string myStreamName)
+        {
+            var deadline = DateTime.UtcNow + TimeSpan.FromMinutes(10);
+            while (DateTime.UtcNow < deadline)
+            {
+                DescribeStreamRequest describeStreamReq = new DescribeStreamRequest();
+                describeStreamReq.StreamName = myStreamName;
+                var describeResult = await kinesisClient.DescribeStreamAsync(describeStreamReq);
+                string streamStatus = describeResult.StreamDescription.StreamStatus;
+                Console.Error.WriteLine("  - current state: " + streamStatus);
+                if (streamStatus == StreamStatus.ACTIVE)
+                {
+                    return;
+                }
+                Thread.Sleep(TimeSpan.FromSeconds(20));
+            }
+
+            throw new Exception("Stream " + myStreamName + " never went active.");
+        }
+
+
+    }
+}
